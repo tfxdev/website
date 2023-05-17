79cd841b944cb39ed64e4366e36237e60a0af947
TouficAhammed <toufic064@gmail.com>
2023-05-17 20:09:16 +0600

    Filter page done

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,12 @@
 document.getElementById("open-popup").addEventListener('click', function(){
     document.querySelector(".location-popup").style.display = "block";
+    document.querySelector(".left-col").style.overflow = "hidden";
     
 })
 
 document.getElementById("popup-close-btn").addEventListener('click', function(){
     document.querySelector(".location-popup").style.display = "none";
-    
-})
+    document.querySelector(".left-col").style.overflowY = "scroll";
 
-
-document.getElementById("open-popup2").addEventListener('click', function(){
-    document.querySelector(".location-popup").style.display = "block";
-    
-})
-
-document.getElementById("popup-close-btn").addEventListener('click', function(){
-    document.querySelector(".location-popup").style.display = "none";
     
 })
